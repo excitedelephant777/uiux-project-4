02b5d972f5b12a2697e126915d1f67a8034a4180
excitedelephant777 <114933800+excitedelephant777@users.noreply.github.com>
2022-11-14 22:14:18 -0500

    try moving pictures to public

--- src/App.js
+++ src/App.js
@@ -6,18 +6,18 @@ function App() {
 
   // distance to providence
   const locations = [
-    {city: "Paris", distance: 3473.59, population: 2.161, language: "French", image: 'images/paris.png'},
-    {city: "London", distance: 3306.69, population: 8.982, language: "English", image: 'images/london.png'},
-    {city: "Rome", distance: 4130.35, population: 2.873, language: "Italian", image: 'images/rome.png'},
-    {city: "Lisbon", distance: 3224.57, population: 0.504, language: "Portuguese", image: 'images/lisbon.png'},
-    {city: "Budapest", distance: 4203.42, population: 1.756, language: "Hungarian", image: 'images/budapest.png'},
-    {city: "Berlin", distance: 3818, population: 3.645, language: "German", image: 'images/berlin.png'},
-    {city: "Madrid", distance: 3428.07, population: 3.223, language: "Spanish", image: 'images/madrid.png'},
-    {city: "Amsterdam", distance: 3487.61, population: 0.821, language: "Dutch", image: '/Users/carolynzech/cs1300/uiux-project-4/src/images/amsterdam.png'},
-    {city: "Cophenhagen", distance: 3694.4, population: 0.602, language: "Danish", image: 'images/copenhagen.png'},
-    {city: "Dublin", distance: 3024.09, population: 0.544, language: "English", image: 'images/dublin.png'},
-    {city: "Athens", distance: 4777.72, population: 3.167, language: "Greek", image: 'images/athens.png'},
-    {city: "Stockholm", distance: 3792.18, population: 0.975, language: "Swedish", image: 'images/stockholm.png'}
+    {city: "Paris", distance: 3473.59, population: 2.161, language: "French", image: '../public/amsterdam.png'},
+    {city: "London", distance: 3306.69, population: 8.982, language: "English", image: '../public/london.png'},
+    {city: "Rome", distance: 4130.35, population: 2.873, language: "Italian", image: '../public/rome.png'},
+    {city: "Lisbon", distance: 3224.57, population: 0.504, language: "Portuguese", image: '../public/lisbon.png'},
+    {city: "Budapest", distance: 4203.42, population: 1.756, language: "Hungarian", image: '../public/budapest.png'},
+    {city: "Berlin", distance: 3818, population: 3.645, language: "German", image: '../public/berlin.png'},
+    {city: "Madrid", distance: 3428.07, population: 3.223, language: "Spanish", image: '../public/madrid.png'},
+    {city: "Amsterdam", distance: 3487.61, population: 0.821, language: "Dutch", image: '..public/amsterdam.png'},
+    {city: "Cophenhagen", distance: 3694.4, population: 0.602, language: "Danish", image: '../public/copenhagen.png'},
+    {city: "Dublin", distance: 3024.09, population: 0.544, language: "English", image: '../public/dublin.png'},
+    {city: "Athens", distance: 4777.72, population: 3.167, language: "Greek", image: '../public/athens.png'},
+    {city: "Stockholm", distance: 3792.18, population: 0.975, language: "Swedish", image: '../public/stockholm.png'}
   ];
 
   return (
